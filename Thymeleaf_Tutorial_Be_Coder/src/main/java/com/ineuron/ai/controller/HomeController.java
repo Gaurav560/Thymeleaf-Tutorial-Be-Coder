7b69843b99b92a4ffea11eb8ac2c2f17e154510f
GAURAV SHARMA <gsharma1154@gmail.com>
2023-11-10 23:36:12 +0530

    fetching dynamic value through iteration with for each loop with th:each

--- Thymeleaf_Tutorial_Be_Coder/src/main/java/com/ineuron/ai/controller/HomeController.java
+++ Thymeleaf_Tutorial_Be_Coder/src/main/java/com/ineuron/ai/controller/HomeController.java
@@ -2,6 +2,9 @@ package com.ineuron.ai.controller;
 
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -27,7 +30,25 @@ public class HomeController {
 
 
 @GetMapping("/profile")
-public String profile() {
+public String profile(Model m1) {
+	User user1=new User(1, "amar", "gaya");
+	User user2=new User(2, "gaurav", "gaya");
+	User user3=new User(3, "sharma", "gaya");
+	User user4=new User(4, "ar", "gaya");
+	User user5=new User(5, "rocky", "gaya");
+	User user6=new User(6, "keyyy", "gaya");
+	User user7=new User(7, "rjvrn", "gaya");
+	
+	ArrayList<User> list=new ArrayList<>();
+	list.add(user1);
+	list.add(user2);
+	list.add(user3);
+	list.add(user4);
+	list.add(user5);
+	list.add(user6);
+	list.add(user7);
+
+	m1.addAttribute("userList", list);
 	
 	return "profile";
 }
