15bde2fced16584831f257f306ccd49b51e47c00
GAURAV SHARMA <gsharma1154@gmail.com>
2023-11-12 07:54:14 +0530

    doctor crud rest api started

---
+++ DoctorCrudOperationRestApi/src/main/java/com/ineuron/ai/DoctorCrudOperationRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.ineuron.ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoctorCrudOperationRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoctorCrudOperationRestApiApplication.class, args);
+	}
+
+}
