2d759e2a1ae47741301e09e91fc1eee4f9df499d
GAURAV SHARMA <gsharma1154@gmail.com>
2023-12-04 23:49:14 +0530

    learned about sorted(),stream(),Stream Interface,map(),reduce(),filter(),Predicate and Function Interface and Lambda expressions

--- StreamApi/src/StreamApiTelusko/Demo.java
+++ StreamApi/src/StreamApiTelusko/Demo.java
@@ -2,48 +2,87 @@ package StreamApiTelusko;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class Demo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		
-		//a new way of adding arrays in the list
-		List<Integer> nums=Arrays.asList(4,6,4,3,2,2,6,7);
-		//System.out.println(nums);
-		
-		int sum=0;
-		//using enhanced for loop
-		for(int n:nums) {
-			if (n%2==0) {
-				n=n*2;
-				sum=sum+n;
-			}
+		// way of adding integer elements in the list of Integers called nums
+		List<Integer> nums = Arrays.asList(3, 5, 9, 2, 8, 11);
+
+		// we have created a stream of integers from the list we had i.e,nums
+	//	Stream<Integer> str = nums.stream();
+		// str.forEach(n->System.out.println(n));
+
+		// long l1=str.count();
+
+		// System.out.println(l1);
+
+		// parallelStream() -when u have huge amount of data ,and we want to do the
+		// processing
+		// of data using multiple threads,we call parallelStream() method.
+
+		// now we have a stream of data, we can process the data based on our
+		// requirements
+
+		// sorted() method returns a new stream
+		// Stream<Integer> data=str.sorted();
+		// data.forEach(n->System.out.println(n));
+
+		// double the value inside the list using normal for loop
+		for (int n : nums) {
+			System.out.println(n * 2);
+
+			// but in this case,we are modifying the original data
+
+
 		}
+		
+//		Stream<Integer> mappedStream=str.map(n->n*2);
+//		mappedStream.forEach(n->System.out.println(n));
+		
+		
+		//or
+		//also called builder pattern
+		//we are using 4 stream here  
+		//1st stream is created by nums.stream which is converting 
+		//the list into stream ,then we are applying filter method on 
+		//1st stream which filters all the odd values and creates a new stream 
+		//which is our 2nd stream ,then we apply sorted() on 2nd stream which sorts the
+		//value by default in ascending order.thus creating a 3rd stream on which we apply
+		//map and performs a function that double the values present in 3rd stream.
+		//thus resulting in 4th stream which is then consumed by forEach() for printing the values.
+		//every time ,the previous stream gets destroyed,so no memory overhead.
+//		 nums.stream()
+//		 .filter(n->n%2==1)
+//		 .sorted()
+//		 .map(n->n*2)
+//		 .forEach
+//		 (n->System.out.println(n));
+		
+		
+		//using reduce()
+		//it takes two parameter.first one is single parameter 
+	//second one is binary parameter
+		//this method reduces the stream in single value
+		
+		
+	Integer sum= nums.stream()
+		 .filter(n->n%2==1)
+		 .sorted()
+		 .map(n->n*2)
+		 .reduce(0,(c,e)->c+e);
+	System.out.println(sum);
 	
-		
-	//	System.out.println(sum);
-		
-		
-		
-		//this is one way of performing operation on your list.
-		//but in this process,original value gets changed.
-		//instead of this ,we use stream API to perform operations on our collections api
+	
+		// map says ,i will give the value but you want to tell what u have to with the
+		// data
+		//map itself gives u a new Stream which u can use further
 		
 		
-		
-		
-		
-		//Different types of for loop(normal,enhanced,forEach)
-		//in the enhanced for loop,the array returns every time
-		//a value and it go and sits in the variable which we define in the parameter
-		//section of for loop
-		
-		
-		//we are going to use for Each in the below example
-		nums.forEach(n->System.out.println(n));
-		
+
 	}
 
 }
